97c15ddc50b4e6ce4ca3ab55d86dc80a3a7f850f
Gregory Johnson <gj@andrew.cmu.edu>
2020-09-10 17:46:08 -0700

    Initial commit

---
+++ samm_rdp/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for SAMM Raw Data Processing."""
+
+__author__ = "Molly Blank"
+__email__ = "molly@shiftlabs.com"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.0"
+
+
+def get_module_version():
+    return __version__
+
+
+from .example import Example  # noqa: F401
